f4c6b752eaed5e004275e4e91c2cabd49fa99485
Thomas Marinier <82789984+th0to@users.noreply.github.com>
2026-06-24 14:56:44 +0200

    Update generate_certs.sh

--- nginx/generate_certs.sh
+++ nginx/generate_certs.sh
@@ -3,4 +3,4 @@ mkdir -p "$(dirname "$0")/certs"
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
   -keyout "$(dirname "$0")/certs/key.pem" \
   -out "$(dirname "$0")/certs/cert.pem" \
-  -subj "/CN=stock.entreprise.local"
+  -subj "/CN=stock.gvaprintlab.ch"
